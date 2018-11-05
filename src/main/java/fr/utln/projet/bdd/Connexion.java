10c02fa8d61914dec3f458b72a07f4ef3649ffb9
Frolz83 <frolz83@gmail.com>
2018-11-05 15:47:10 +0100

    creation de la classe de connexion

--- src/main/java/fr/utln/projet/bdd/Connexion.java
+++ src/main/java/fr/utln/projet/bdd/Connexion.java
@@ -1,5 +1,19 @@
 package fr.utln.projet.bdd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connexion {
-    
+    private Connection conn;
+    private String host = "127.0.0.1";
+
+    public void connect(){
+        try{
+            conn=DriverManager.getConnection("jdbc:postgresql://"+host+"/2000/sinfo1");
+            System.out.println("Connexion ok");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
