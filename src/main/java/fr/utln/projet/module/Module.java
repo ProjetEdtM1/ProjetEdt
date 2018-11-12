9fa7775758695d13539914f2d6bc30f35ad65a79
Marcnocito <marc.nocito@hotmail.fr>
2018-11-12 15:46:44 +0100

    creation de la classe module

---
+++ src/main/java/fr/utln/projet/module/Module.java
@@ -0,0 +1,46 @@
+package fr.utln.projet.module;
+
+public class Module {
+    public String intitule;
+    public int nbHeureCm;
+    public int nbHeureTd;
+    public int nbHeureTP;
+
+    public static class Builder {
+        private final String intitule;
+
+        private int nbHeureCm = 0;
+        private int nbHeureTd = 0;
+        private int nbHeureTp = 0;
+
+        public Builder(String intitule) {
+            this.intitule = intitule;
+        }
+
+        public Builder nbHeureCm(int nbHeureCm) {
+            this.nbHeureCm = nbHeureCm;
+            return this;
+        }
+
+        public Builder nbHeureTd(int nbHeureTd) {
+            this.nbHeureTd = nbHeureTd;
+            return this;
+        }
+
+        public Builder nbHeureTp(int nbHeureTp) {
+            this.nbHeureTp = nbHeureTp;
+            return this;
+        }
+
+        public Module build() {
+            return new Module(this);
+        }
+    }
+
+    private Module(Builder builder) {
+        intitule = builder.intitule;
+        nbHeureCm = builder.nbHeureCm;
+        nbHeureTd = builder.nbHeureTd;
+        nbHeureTP = builder.nbHeureTp;
+    }
+}
