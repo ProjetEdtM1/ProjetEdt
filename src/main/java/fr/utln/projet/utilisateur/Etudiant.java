e51ba61451d5090b113d2c19a83bfaaf5124f524
CLAIN CYRIL <claincyril0@gmail.com>
2018-11-14 11:48:20 +0100

    ajout classe etudiant

--- src/main/java/fr/utln/projet/utilisateur/Etudiant.java
+++ src/main/java/fr/utln/projet/utilisateur/Etudiant.java
@@ -1,4 +1,106 @@
 package fr.utln.projet.utilisateur;
 
+/*
+ * Nom de classe : Etudiant
+ *
+ * Description   : Classe Etudiant qui comporte les données de celui-ci
+ *
+ * Version       : 1.2
+ *
+ * Date          : 08/11/2018
+ *
+ * Copyright     : CLAIN Cyril
+ */
+
+import java.util.Objects;
+
 public class Etudiant {
+    private String Nom;
+    private String Prenom;
+    private String Groupe;
+    public String Numetud;
+    private String mdp;
+    private String telephone;
+
+    public Etudiant() {
+
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public Etudiant(String nom, String prenom, String groupe, String numetud, String mdp, String telephone) {
+        this.mdp = mdp;
+        this.Nom = nom;
+        this.Prenom = prenom;
+        this.Groupe = groupe;
+        this.Numetud = numetud;
+        this.telephone = telephone;
+    }
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public String getPrenom() {
+        return Prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        Prenom = prenom;
+    }
+
+    public String getGroupe() {
+        return Groupe;
+    }
+
+    public void setGroupe(String groupe) {
+        Groupe = groupe;
+    }
+
+    public String getNumetud() {
+        return Numetud;
+    }
+
+    public void setNumetud(String numetud) {
+        Numetud = numetud;
+    }
+
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "Nom='" + Nom + '\'' +
+                ", Prenom='" + Prenom + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Etudiant etudiant = (Etudiant) o;
+        return Objects.equals(Numetud, etudiant.Numetud);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Numetud);
+    }
 }
