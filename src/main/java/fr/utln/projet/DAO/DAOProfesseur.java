89090f9b0646bf66356f77df5d1abf32c4fd3359
cclain594 <claincyril0@gmail.com>
2018-11-16 20:53:34 +0100

    ajout methode create list en bdd

--- src/main/java/fr/utln/projet/DAO/DAOProfesseur.java
+++ src/main/java/fr/utln/projet/DAO/DAOProfesseur.java
@@ -1,10 +1,15 @@
 package fr.utln.projet.DAO;
 
 import fr.utln.projet.bdd.Connexion;
+import fr.utln.projet.utilisateur.Etudiant;
+import fr.utln.projet.utilisateur.Professeur;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DAOProfesseur {
     private Connexion conn;
@@ -39,5 +44,44 @@ public class DAOProfesseur {
         }
         return false;
 
+    }
+
+    /**
+     * Methode de creation d'une liste de professeur ( présent en BD)
+     *
+     * @return liste d'etudiant
+     *
+     * @author CLAIN Cyril
+     */
+    public List<Professeur> creationListProfesseur(){
+
+        // debut de connection
+        this.conn = new Connexion();
+        conn.connect();
+        List<Professeur> professeurList = new ArrayList<Professeur>();
+        //requette de selection des Etudiant
+        String sql = "SELECT * FROM PROFESSEUR";
+        try {
+            Statement statementSelectall = conn.getConn().createStatement();
+            ResultSet resListeDesProfesseur = statementSelectall.executeQuery(sql);
+            while(resListeDesProfesseur.next()){
+                Professeur professeur = new Professeur();
+                professeur.setIdprofesseur(resListeDesProfesseur.getString(1));
+                professeur.setNom(resListeDesProfesseur.getString(2));
+                professeur.setPrenom(resListeDesProfesseur.getString(3));
+                professeur.setLogin(resListeDesProfesseur.getString(4));
+                professeur.setMdp(resListeDesProfesseur.getString(5));
+                professeurList.add(professeur);
+            }
+
+            statementSelectall.close();
+            conn.close();
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return professeurList;
     }
 }
