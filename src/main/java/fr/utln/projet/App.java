f3a15c2aea98193971bfa164de23a60a4e758c18
CLAIN CYRIL <claincyril0@gmail.com>
2018-11-19 15:20:58 +0100

    debut modification professeur

--- src/main/java/fr/utln/projet/App.java
+++ src/main/java/fr/utln/projet/App.java
@@ -23,11 +23,11 @@ public class App
 {
     public static void main( String[] args ) throws SQLException {
 
-        ModeleEtudiant modeleEtudiant = new ModeleEtudiant();
-        MenuProfRefVue menuProfRefVue = new MenuProfRefVue("cyril");
-        EtudiantVue etudiantVue = new EtudiantVue(modeleEtudiant, menuProfRefVue);
-        //ProfesseurModele professeurModele = new ProfesseurModele();
-        //ProfesseurVue professeurVue = new ProfesseurVue(professeurModele);
+        //ModeleEtudiant modeleEtudiant = new ModeleEtudiant();
+       // MenuProfRefVue menuProfRefVue = new MenuProfRefVue("cyril");
+        //EtudiantVue etudiantVue = new EtudiantVue(modeleEtudiant, menuProfRefVue);
+        ProfesseurModele professeurModele = new ProfesseurModele();
+        ProfesseurVue professeurVue = new ProfesseurVue(professeurModele);
         AuthentificationVue authentificationVue = new AuthentificationVue();
         //ModuleModele moduleModele = new ModuleModele();
         //ModuleVUE moduleVUE = new ModuleVUE(moduleModele);
