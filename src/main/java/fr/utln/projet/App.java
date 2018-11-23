21d418c308b0a61f9be10219cf310b47355ed004
CLAIN CYRIL <claincyril0@gmail.com>
2018-11-23 11:37:33 +0100

    main

--- src/main/java/fr/utln/projet/App.java
+++ src/main/java/fr/utln/projet/App.java
@@ -1,19 +1,9 @@
 package fr.utln.projet;
 
-import fr.utln.projet.DAO.DAOEtudiant;
-import fr.utln.projet.DAO.DAOProfesseur;
-import fr.utln.projet.bdd.Connexion;
-import fr.utln.projet.modele.ModuleModele;
-import fr.utln.projet.modele.ProfesseurModele;
-import fr.utln.projet.module.Module;
-import fr.utln.projet.utilisateur.Professeur;
-import fr.utln.projet.utilisateur.Utilisateur;
-import fr.utln.projet.vue.*;
-import fr.utln.projet.modele.ModeleEtudiant;
 
-import java.sql.ResultSet;
+import fr.utln.projet.vue.*;
+
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * Hello world!
@@ -27,9 +17,8 @@ public class App
         //EtudiantVue etudiantVue = new EtudiantVue(modeleEtudiant);
         //ProfesseurModele professeurModele = new ProfesseurModele();
         //ProfesseurVue professeurVue = new ProfesseurVue(professeurModele);
-//        AuthentificationVue authentificationVue = new AuthentificationVue();
+//         AuthentificationVue authentificationVue = new AuthentificationVue();
 //        ModuleModele moduleModele = new ModuleModele();
         MenuProfRefVue a = new MenuProfRefVue("coucou");
-
 
     }}
