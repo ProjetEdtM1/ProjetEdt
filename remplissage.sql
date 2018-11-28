99d351de6784b6d4ebf7d8e1fbe554a96ce2caa6
Marcnocito <marc.nocito@hotmail.fr>
2018-11-28 18:52:17 +0100

    merge remplissage

--- remplissage.sql
+++ remplissage.sql
@@ -7,3 +7,10 @@ insert into GROUPE (intituleGroupe, intitulePromo) values ('SI1A', 'SI');
 
 insert into ETUDIANT (numEtudiant, nomEtudiant, prenomEtudiant, motDePasse, intituleGroupe)
   values ('210323', 'DUPONT', 'RENE', 'confiture', 'SI1A');
+
+insert into SALLE (numeroSalle) values (1);
+
+insert into RESERVER (numerosalle, idprofesseur, datereservation, heuredebres, etat)
+  values (1, 1, curdate(), curtime(), 1);
+insert into RESERVER (numerosalle, idprofesseur, datereservation, heuredebres, heureFinRes, etat)
+  values (1,1, '2018-01-01', '10:00:00', '12:00:00', 0)
